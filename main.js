c5cd62ff65dd162b3c2c5dc4c9034764324d2845
NeeharSheth <75029741+NeeharSheth@users.noreply.github.com>
2021-07-09 14:49:22 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function modelLoaded(){
+    console.log("Model Loaded :D");
+}
+
+function preload(){
+
+}
+
+function setup(){
+    canvas= createCanvas(300,300);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.hide();
+    classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/hymotIPM4/model.json",modelLoaded);
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    classifier.classify(video, gotResult);
+}
+
+function gotResult(error,result){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(result);
+    document.getElementById("result_object").innerHTML= result[0].label;
+    document.getElementById("result_acc").innerHTML= result[0].confidence.toFixed(2)*100+"%";
+    }
+}
